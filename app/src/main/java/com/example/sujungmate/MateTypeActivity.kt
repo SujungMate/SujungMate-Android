20ffaacac40d387d59fc176c610e8c72e1f43ef3
Jeonghoo Son <ellien0311@naver.com>
2021-08-04 17:08:48 +0900

    수정: 매칭 유형 선택 액티비티

--- app/src/main/java/com/example/sujungmate/MateTypeActivity.kt
+++ app/src/main/java/com/example/sujungmate/MateTypeActivity.kt
@@ -7,5 +7,7 @@ class MateTypeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mate_type)
+
+        //매칭 유형 선택 액티비티
     }
 }
