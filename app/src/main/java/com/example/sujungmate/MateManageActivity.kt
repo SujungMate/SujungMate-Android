deb64588514e05b96f872e32ffbe7273986c9107
b1urrrr <b1urrr@naver.com>
2021-08-11 15:48:03 +0900

    작업 : InfoModifyActivity, MateManageActivity, MyPageActivity, PWChangeActivity 추가

---
+++ app/src/main/java/com/example/sujungmate/MateManageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sujungmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MateManageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mate_manage)
+    }
+}
