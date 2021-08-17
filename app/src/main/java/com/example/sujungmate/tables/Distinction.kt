def75e9920777c90b1b8bd0d079b8c2cf13820b0
b1urrrr <b1urrr@naver.com>
2021-08-17 15:50:51 +0900

    작업 : signUp2~4 Distinction(특징 데이터) 저장 구현

---
+++ app/src/main/java/com/example/sujungmate/tables/Distinction.kt
@@ -0,0 +1,9 @@
+package com.example.sujungmate.tables
+
+data class Distinction(var studentID : Long? = null,
+                       var nickname : String? = null,
+                       var lecture : String? = null,
+                       var major : String? = null,
+                       var interest : String? = null,
+                       var mbti : String? = null) {
+}
