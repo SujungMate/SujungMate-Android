b8238235aef9e81c79ab93dc69efe72b955c7c8d
b1urrrr <b1urrr@naver.com>
2021-08-19 08:05:18 +0900

    작업 : 내 정보 수정, 비밀번호 변경 구현중

--- app/src/main/java/com/example/sujungmate/PWChangeActivity.kt
+++ app/src/main/java/com/example/sujungmate/PWChangeActivity.kt
@@ -3,13 +3,29 @@ package com.example.sujungmate
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.sujungmate.tables.Users
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_my_page.*
+import kotlinx.android.synthetic.main.activity_pwchange.*
 
 class PWChangeActivity : AppCompatActivity() {
+    companion object {
+        var currentUser: Users? = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pwchange)
@@ -25,6 +41,23 @@ class PWChangeActivity : AppCompatActivity() {
         val outer_layout = findViewById<ConstraintLayout>(R.id.layout_pwchange) // 레이아웃 가져오기
         outer_layout.setOnClickListener {
             hideKeyboard()
+        }
+
+        // Toast.makeText(this," ", Toast.LENGTH_SHORT).show()
+
+        modify_pwchange.setOnClickListener {
+            val EMAIL = email_pwchange.text.toString()
+
+            if (EMAIL.equals("")) {
+                notice_pwchange.text = "이메일을 입력하지 않았습니다."
+            } else {
+                Firebase.auth.sendPasswordResetEmail(EMAIL)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            notice_pwchange.text = "비밀번호 변경 메일이 전송되었습니다."
+                        }
+                    }
+            }
         }
     }
 
@@ -43,12 +76,8 @@ class PWChangeActivity : AppCompatActivity() {
 
     //키보드 숨기기
     fun hideKeyboard() {
-        val editText1 = findViewById<EditText>(R.id.oldPW_pwchange)
-        val editText2 = findViewById<EditText>(R.id.newPW_pwchange)
-        val editText3 = findViewById<EditText>(R.id.newPW2_pwchange)
+        val editText1 = findViewById<EditText>(R.id.email_pwchange)
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(editText1.windowToken, 0)
-        imm.hideSoftInputFromWindow(editText2.windowToken, 0)
-        imm.hideSoftInputFromWindow(editText3.windowToken, 0)
     }
 }
